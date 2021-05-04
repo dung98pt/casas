0def92ed63484a104638dc556854b6130c38b972
dung98pt <dung98pt@gmail.com>
2021-05-04 06:19:04 +0000

    load

--- train/keras_train.py
+++ train/keras_train.py
@@ -13,10 +13,11 @@ label_encoder = LabelEncoder()
 from sklearn.model_selection import train_test_split
 from model.keras_model import get_model
 import data
+import tensorflow as tf
 
 seed = 7
 units = 64
-epochs = 1
+epochs = 100
 args_model = "biLSTM"
 datasetName = "cairo"
 maxLength = 2000
@@ -40,7 +41,7 @@ if __name__ == '__main__':
         X_test_input = X_test
     no_activities = len(dictActivities)
     model = get_model(args_model, input_dim, no_activities, maxLength)
-    # model.load_weights("model/model_1-cairo-20210409-103533.h5")
+    model.load_weights("logging/log_case_1/training/model/biLSTM-cairo-20210503-034847.h5")
     modelname = model.name
     # checkpoint callback
     currenttime = datetime.utcnow().strftime('%Y%m%d-%H%M%S')
@@ -59,6 +60,5 @@ if __name__ == '__main__':
         class_weight[i] = num_sample/num_class/list(Y_train).count(i)
     print("class_weight: ", class_weight)
     # class_weight = compute_class_weight('balanced', np.unique(Y), Y)  # use as optional argument in the fit function
-    model.fit(X_train_input, Y_train, validation_split=0.2, epochs=1, batch_size=64, verbose=1)
-    model.fit(X_train_input, Y_train, validation_data = (X_test_input, Y_test), epochs=epochs, batch_size=128, verbose=1, class_weight = class_weight,
-              callbacks=[csv_logger, model_checkpoint])
+    with tf.device("/gpu:0"):
+      model.fit(X_train_input, Y_train, validation_data = (X_test_input, Y_test), epochs=epochs, batch_size=256, verbose=1, class_weight = class_weight, callbacks=[csv_logger, model_checkpoint])
